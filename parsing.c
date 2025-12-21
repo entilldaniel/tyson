0cd4693ee791ba49ff5df03302796d3e0b5f5ccc
Daniel Figueroa <daniel.whalenut@gmail.com>
2025-12-21 19:24:55 +0100

    Initial Commit.
    This is the first commit of my own lisp, it follows a tutorial found
    here: https://www.buildyourownlisp.com/chapter6_parsing
    
    mpc.h and mpc.c are borrowed from here:
    https://github.com/orangeduck/mpc
    
    The license will be included in the next commit.
    
    We'll se if I in the future will make this programming language to
    something more or if I'll just leav it be.
    
    In either case, if you're reading this thanks for your interest.

---
+++ parsing.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+#include "mpc.h"
+
+
+int main(int argc, char** argv) {
+  
+  /* Creating some parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+	      "                                                    \
+		    number   : /-?[0-9]+/ ;				               \
+            operator : '+' | '-' | '*' | '/';                  \
+            expr     : <number> | '(' <operator> <expr>+ ')' ; \
+		    lispy    : /^/ <operator> <expr>+ /$/ ;            \
+		  ",
+			Number, Operator, Expr, Lispy);
+ 
+  puts("Lispy Version 0.0.0.1");
+  puts("Press Ctrl-C to Exit\n");
+
+  while (1) {
+	char* input = readline("lispy> ");
+	add_history(input);
+	printf("No you're a %s\n", input);
+	free(input);
+  }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  return 0;
+}
+
